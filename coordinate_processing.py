052919bc5ab8df917dbb76a801b1b8fb61072d56
ahatfield <andrewscotthat@gmail.com>
2021-12-16 06:55:04 -0800

    Updates the processing script to output x/y coordinates in addition to z. This processing currently loses a lot of points, but I think it will give a decent x/y for the points it does have.

--- coordinate_processing.py
+++ coordinate_processing.py
@@ -1,7 +1,13 @@
 import argparse
 import os
 import sys
+from collections import namedtuple
+from statistics import mean
 
+Snap = namedtuple("Snap", "id angle x y")
+Coord = namedtuple("Coord", "id x y z")
+
+IMAGE_WIDTH = 1080
 
 def main():
     parser = argparse.ArgumentParser()
@@ -18,12 +24,12 @@ def main():
         lines = [line.rstrip() for line in lines]
 
     coordinates = {}
-    shot = []
+    shot = {}
 
     # Parse the lines and filter ones we don't want to consider.
     for l in lines:
         # print(l)
-        id = int(l[2:5])
+        led_id = int(l[2:5])
         # print(f"id {id}")
         angle = int(l[11:14])
         # print(f"angle {angle}")
@@ -33,28 +39,43 @@ def main():
         # print(f"y {y}")
         if not x or not y:
             continue
-        if angle in [45, 135, 225, 315]:
-            continue
 
-        shot.append((int(id), int(angle), int(x), int(y)))
+        shot.setdefault(led_id, []).append(Snap(led_id, angle, x, y))
 
     # Calculate the coordinates
-    for s in shot:
-        if s[0] in coordinates:
-            prev = coordinates[s[0]]
-            if abs(540 - s[2]) < abs(540 - prev[2]):
-                coordinates[s[0]] = s
+    missing_count = 0
+    for k in shot.keys():
+        led_id = k
+
+        filtered_list = list(filter(lambda s: s.x != 0 and s.y != 0, shot[k]))
+
+        z = mean(map(lambda s: s.y, shot[k]))
+
+        ys = list(filter(lambda s: s.angle in [90, 270], filtered_list))
+        if not ys:
+            missing_count += 1
+            continue
         else:
-            coordinates[s[0]] = s
+            y = ys[0].x if ys[0].angle == 270 else IMAGE_WIDTH - ys[0].x
+
+        xs = list(filter(lambda s: s.angle in [0, 180], filtered_list))
+        if not xs:
+            missing_count += 1
+            continue
+        else:
+            x = xs[0].x if xs[0].angle == 0 else IMAGE_WIDTH - xs[0].x
+
+        coordinates[led_id] = Coord(led_id, x, y, int(z))
 
     # Write the output to file
     with open(args.output_file, 'w') as output_file:
         output_file.write("#")
         for k, v in coordinates.items():
-            c = f"{k},{v[2]},0,{v[3]}"
+            c = f"{v.id},{v.x},{v.y},{v.z}"
             output_file.write(c + "\n")
             print(c)
 
+    print(f"Missing elements: {missing_count}")
 
 # Main program logic follows:
 if __name__ == '__main__':
