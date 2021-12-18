fe6ecab8d91ed506218e06ec32feae82c675cc53
Andrew <andrewscotthat@gmail.com>
2021-12-17 16:41:34 -0800

    Updates the animation to be more fluid.

--- animation.py
+++ animation.py
@@ -9,7 +9,7 @@ LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN       = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 100  # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 150  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
@@ -27,7 +27,7 @@ def fill(strip, color=LED_OFF):
 
 
 # Define functions which animate LEDs in various ways.
-def fillByHeight(strip, coordinates):
+def fillByHeight(strip, coordinates, axis="x"):
     speed = 2
     width = 300
     half_band = width / 2
@@ -37,10 +37,14 @@ def fillByHeight(strip, coordinates):
     for i in range(0, width * 2, speed):
         for ledid, coord in coordinates.items():
             # Distance from the given coordinate
-            x = coord[0]
-            z = coord[2]
+            if axis == "x":
+                d = coord[0]
+            elif axis == "y":
+                d = coord[1]
+            elif axis == "z":
+                d = coord[2]
 
-            dist = (z - i) % (width * 2)
+            dist = (d - i) % (width * 2)
 
             is_red = 0 < dist < width
 
@@ -60,6 +64,7 @@ def main():
     # Process arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input-file', type=str, help='The file to read in.')
+    parser.add_argument('-x', '--axis', type=str, help='The axis to run the animation around')
     args = parser.parse_args()
 
     # Create NeoPixel object with appropriate configuration.
@@ -86,7 +91,7 @@ def main():
         print('Press Ctrl-C to quit.')
         while True:
             # fill(strip, LED_WHITE)
-            fillByHeight(strip, coordinates)
+            fillByHeight(strip, coordinates, axis=args.axis)
 
     except KeyboardInterrupt:
         # Catch interrupt
